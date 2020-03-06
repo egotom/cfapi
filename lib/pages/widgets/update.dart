ada54b0c8f5c3985e2482dbaaeb8d2f651aa6f19
egotom <egotom@gmail.com>
2020-03-06 16:24:29 +0800

    skt

---
+++ lib/pages/widgets/update.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final _controller = TextEditingController();
+
+  void initState() {
+    _controller.addListener(() {
+      final text = _controller.text.toLowerCase();
+      _controller.value = _controller.value.copyWith(
+        text: text,
+        selection:
+            TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
+    super.initState();
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(6),
+        child: TextFormField(
+          controller: _controller,
+          decoration: InputDecoration(border: OutlineInputBorder()),
+        ),
+      ),
+    );
+  }
+}
